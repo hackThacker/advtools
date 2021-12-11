58f0744719c1189d40c430ee685bcefab9bac3f5
n££®æj <67623286+hackThacker@users.noreply.github.com>
2021-12-11 12:04:22 +0545

    Update Android_tools.py

--- tools/Android_tools.py
+++ tools/Android_tools.py
@@ -5,7 +5,7 @@ from core import HackingTool
 from core import HackingToolsCollection
 
 class Androidtools(HackingTool):
-    TITLE = "Android_tools"
+    TITLE = "Androidtools"
     DESCRIPTION = "Mobile Verification Toolkit (MVT) is a collection of utilities to simplify and automate the process." \
                   "  of gathering forensic traces helpful to identify a potential compromise of Android and iOS devices."
    INSTALL_COMMANDS = [
@@ -19,7 +19,7 @@ class Androidtools(HackingTool):
         super(Androidtools, self).__init__([('Stop', self.stop)])
 
     def stop(self):
-        os.system("sudo Android_tools stop")
+        os.system("sudo Androidtools stop")
         
 class Androidtools(HackingToolsCollection):
     TITLE = "Android Tools"
