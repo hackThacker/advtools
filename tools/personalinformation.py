590e60f566b69ecd28fe26a0a2b205bf488dce1d
n££®æj <67623286+hackThacker@users.noreply.github.com>
2022-07-13 12:10:52 +0545

    Add files via upload

---
+++ tools/personalinformation.py
@@ -0,0 +1,43 @@
+import os
+
+from core import HackingTool
+from core import HackingToolsCollection
+
+class DaProfiler(HackingTool):
+    TITLE = "DaProfiler"
+    DESCRIPTION = "DaProfiler is an OSINT tool allowing you to collect certain information about\n" \
+                  "Addresses, Social media accounts, e-mail addresses, mobile / landline number, jobs."
+    INSTALL_COMMANDS = [
+        "git clone https://github.com/TheRealDalunacrobate/DaProfiler.git",
+        "cd DaProfiler && pip3 install -r requirements.txt"
+    ]
+    RUN_COMMANDS = ["sudo  python profiler.py -h"]
+    PROJECT_URL = " https://github.com/TheRealDalunacrobate/DaProfiler"
+    
+    
+class Profil3r(HackingTool):
+    TITLE = "Profil3r"
+    DESCRIPTION = "DaProfiler is an OSINT tool allowing you to collect certain information about\n" \
+                  "Addresses, Social media accounts, e-mail addresses, mobile / landline number, jobs."
+    INSTALL_COMMANDS = [
+        "pip3 install PyInquirer jinja2",
+        " git clone https://github.com/Rog3rSm1th/Profil3r.git",
+        "cd Profil3r/ ",
+        "sudo python3 setup.py install"
+    ]
+    RUN_COMMANDS = ["python3 profil3r.py -h"]
+    PROJECT_URL = " https://github.com/TheRealDalunacrobate/DaProfiler"
+    
+    
+    
+    
+def __init__(self):
+        super(Profil3r, self).__init__(installable = False, runnable = False)
+
+
+class userinformation(HackingToolsCollection):
+    TITLE = "personal information gather"
+    TOOLS = [
+DaProfiler(),
+Profil3r()
+    ]
