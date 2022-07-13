6e56dbb515d57361e5b6bd33f242ecfafbcc1d73
n££®æj <67623286+hackThacker@users.noreply.github.com>
2022-07-13 09:30:21 +0545

    Update phising_attack.py

--- tools/phising_attack.py
+++ tools/phising_attack.py
@@ -109,7 +109,7 @@ class WishFish(HackingTool):
                   "[+]./ngrok authtoken Your_auth_token \n\n\t " \
                   "[+]./ngrok http 80 \n " \
                   "[+]then ngrok server will run and host on https and http \n" \
-    INSTALL_COMMANDS = ["sudo git clone https://github.com/kinghacker0/WishFish.git"]
+    INSTALL_COMMANDS = ["git clone https://github.com/kinghacker0/WishFish.git"]
     RUN_COMMANDS = ["sudo cd WishFish;sudo bash wishfish.sh"]
     PROJECT_URL = "https://github.com/kinghacker0/WishFish"
 
