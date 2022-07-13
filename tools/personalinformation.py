375f258417fb90b2b4a525d295b22724a27bc1c9
n££®æj <67623286+hackThacker@users.noreply.github.com>
2022-07-13 12:27:08 +0545

    Update personalinformation.py

--- tools/personalinformation.py
+++ tools/personalinformation.py
@@ -11,21 +11,21 @@ class DaProfiler(HackingTool):
         "git clone https://github.com/TheRealDalunacrobate/DaProfiler.git",
         "cd DaProfiler && pip3 install -r requirements.txt"
     ]
-    RUN_COMMANDS = ["python profiler.py -h"]
+    RUN_COMMANDS = ["python profiler.py john doe"]
     PROJECT_URL = " https://github.com/TheRealDalunacrobate/DaProfiler"
     
     
 class Profil3r(HackingTool):
     TITLE = "Profil3r"
-    DESCRIPTION = "DaProfiler is an OSINT tool allowing you to collect certain information about\n" \
-                  "Addresses, Social media accounts, e-mail addresses, mobile / landline number, jobs."
+    DESCRIPTION = "Profil3r is an OSINT tool that allows you to find potential profiles of a person on social networks, as well as their email addresses\n" \
+                  "This program also alerts you data leak for the found emails."
     INSTALL_COMMANDS = [
         "pip3 install PyInquirer jinja2",
         " git clone https://github.com/Rog3rSm1th/Profil3r.git",
         "cd Profil3r/ ",
         "sudo python3 setup.py install"
     ]
-    RUN_COMMANDS = ["python3 profil3r.py -h"]
+    RUN_COMMANDS = ["python3 profil3r.py john doe"]
     PROJECT_URL = " https://github.com/TheRealDalunacrobate/DaProfiler"
     
     
