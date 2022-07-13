4fea4f32938b6a0e5248f3ef6d7886268b37c182
n££®æj <67623286+hackThacker@users.noreply.github.com>
2022-07-13 12:52:09 +0545

    Update hackingtool.py

--- hackingtool.py
+++ hackingtool.py
@@ -42,8 +42,10 @@ logo = """\033[33m
                                     \033[34m[✔]  https://github.com/hackThacker/advtools.git [✔]
                                     \033[34m[✔]            Version 1.1.1              [✔]
                                     \033[91m[X] Please Don't Use For illegal Activity  [X]
-                                    \033[91m[X] This is for Education-purpose [X]                                     
-\033[97m"""                                  
+                                    \033[91m[X] This is for Education-purpose [X] 
+                                   \[\033[0;37m\]  Ｔｈｉｓ Ｔｏｏｌ ｉｓ Ｃｒｅａｔｅｄ ｂｙ ：－ ｈａｃｋＴｈａｃｋｅｒ☻
+                                    \033[97m"""  
+                                                         
 all_tools = [
     AnonSurfTools(),
     bughunter(),
